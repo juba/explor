83dfce4b5a63d6d4a1950a6193825bc06b834364
Julien Barnier <julien@nozav.org>
2015-11-26 17:08:01 +0100

    Add FactoMineR datasets examples

--- example.R
+++ example.R
@@ -6,8 +6,8 @@ library(explor)
 data(hdv2003)
 
 d <- hdv2003 %>% 
-  select(sexe, nivetud, qualif, clso, relig, cuisine, bricol, cinema, sport, age, freres.soeurs)
-acm <- MCA(d, quali.sup = 8:9, ind.sup = 1:50, quanti.sup = 10:11, graph = FALSE)
+  select(sexe, qualif, relig, cuisine, bricol, cinema, sport, age, freres.soeurs)
+acm <- MCA(d, quali.sup = 6:7, ind.sup = 1:50, quanti.sup = 8:9, graph = FALSE)
 explor(acm)
 
 d <- hdv2003 %>% 
@@ -16,15 +16,20 @@ acm <- MCA(d, graph = FALSE)
 explor(acm)
 
 
-library(questionr)
 library(FactoMineR)
-library(dplyr)
 library(explor)
 
-data(rp99)
-d <- rp99
-rownames(d) <- rp99$nom
-d$popcl <- cut(d$pop.tot, breaks = c(50,500,1000,5000,10000,100000,125000))
-d <- d %>% select(agric, artis, cadres, interm, empl, ouvr, retr, pop15)
-acp <- PCA(d, scale.unit = TRUE, quanti.sup = 8, ind.sup = 1:20)
-explor(acp)
+data(decathlon)
+d <- decathlon[,1:12]
+pca <- PCA(d, quanti.sup = 11:12, graph = FALSE)
+explor(pca)
+
+data(decathlon)
+d <- decathlon[,1:12]
+pca <- PCA(d, quanti.sup = 11:12, graph = FALSE, scale.unit = FALSE)
+explor(pca)
+
+
+data(hobbies)
+mca <- MCA(hobbies[1:1000,c(1:8,21:23)],quali.sup = 9:10, quanti.sup = 11, ind.sup = 1:100)
+explor(mca)
