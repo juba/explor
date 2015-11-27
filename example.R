03568b0f32273de96064077a0ac3b07b5fb3445c
Julien Barnier <julien@nozav.org>
2015-11-27 14:13:29 +0100

    Update examples

--- example.R
+++ example.R
@@ -24,12 +24,7 @@ d <- decathlon[,1:12]
 pca <- PCA(d, quanti.sup = 11:12, graph = FALSE)
 explor(pca)
 
-data(decathlon)
-d <- decathlon[,1:12]
-pca <- PCA(d, quanti.sup = 11:12, graph = FALSE, scale.unit = FALSE)
-explor(pca)
-
 
 data(hobbies)
-mca <- MCA(hobbies[1:1000,c(1:8,21:23)],quali.sup = 9:10, quanti.sup = 11, ind.sup = 1:100)
+mca <- MCA(hobbies[1:1000,c(1:8,21:23)],quali.sup = 9:10, quanti.sup = 11, ind.sup = 1:100, graph = FALSE)
 explor(mca)
