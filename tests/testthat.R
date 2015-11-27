b674c0adea1be6f3512bc3e65062f263790d656a
Julien Barnier <julien@nozav.org>
2015-11-27 14:12:33 +0100

    Add tests for prepare_results functions

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(explor)
+
+test_check("explor")
---
+++ tests/testthat/test_prepare_results_MCA.R
@@ -0,0 +1,66 @@
+library(FactoMineR)
+context("prepare_results.MCA")
+
+data(hobbies)
+mca <- MCA(hobbies[1:1000,c(1:8,21:23)],quali.sup = 9:10, 
+           quanti.sup = 11, ind.sup = 1:100, graph = FALSE)
+res <- prepare_results(mca)
+
+test_that("Eigenvalues are equals", {
+  expect_equal(mca$eig$`percentage of variance`, res$eig$percent)
+})
+
+test_that("Variables results are equal", {
+  expect_equal(as.vector(signif(mca$var$coord[,1],3)),
+               res$vars[res$vars$Type == "Active" & res$vars$Axis == "1", "Coord"])
+  expect_equal(as.vector(signif(mca$var$cos2[,2],2)),
+               res$vars[res$vars$Type == "Active" & res$vars$Axis == "2", "Cos2"])
+  expect_equal(as.vector(signif(mca$var$contrib[,3],3)),
+               res$vars[res$vars$Type == "Active" & res$vars$Axis == "3", "Contrib"])
+  expect_equal(as.vector(signif(mca$var$v.test[,1],2)),
+               res$vars[res$vars$Type == "Active" & res$vars$Axis == "1", "V.test"])
+  expect_equal(as.vector(signif(mca$var$v.test[,1],2)),
+               res$vars[res$vars$Type == "Active" & res$vars$Axis == "1", "V.test"])
+  expect_equal(as.vector(format(mca$var$eta2[,5], scientific = FALSE, nsmall = 3, digits = 0)),
+               data.frame(res$vareta2)[res$vareta2$Type == "Active" & 
+                                         res$vareta2$Axis == "5", "eta2", drop = TRUE])
+})
+
+test_that("Qualitative supplementary variables results are equal", {
+  expect_equal(as.vector(signif(mca$quali.sup$coord[,1],3)),
+               res$vars[res$vars$Type == "Supplementary" & 
+                          res$vars$Class == "Qualitative" & res$vars$Axis == "1", "Coord"])
+  expect_equal(as.vector(signif(mca$quali.sup$cos2[,2],2)),
+               res$vars[res$vars$Type == "Supplementary" & 
+                          res$vars$Class == "Qualitative" & res$vars$Axis == "2", "Cos2"])
+  expect_equal(as.vector(signif(mca$quali.sup$v.test[,3],2)),
+               res$vars[res$vars$Type == "Supplementary" & 
+                          res$vars$Class == "Qualitative" & res$vars$Axis == "3", "V.test"])
+  expect_equal(as.vector(format(mca$quali.sup$eta2[,2], scientific = FALSE, nsmall = 3, digits = 0)),
+               data.frame(res$vareta2)[res$vareta2$Type == "Supplementary" & 
+                                         res$vars$Class == "Qualitative" &
+                                         res$vareta2$Axis == "2", "eta2", drop = TRUE])
+})
+
+test_that("Quantitative supplementary variables results are equal", {
+  expect_equal(as.vector(signif(mca$quanti.sup$coord[,1],3)),
+               res$vars[res$vars$Type == "Supplementary" & 
+                          res$vars$Class == "Quantitative" & res$vars$Axis == "1", "Coord"])
+})
+
+test_that("Individuals results are equal", {
+  expect_equal(as.vector(signif(mca$ind$coord[,1],3)),
+               data.frame(res$ind)[res$ind$Type == "Active" & res$ind$Axis == "1", "Coord"])
+  expect_equal(as.vector(signif(mca$ind$contrib[,3],3)),
+               data.frame(res$ind)[res$ind$Type == "Active" & res$ind$Axis == "3", "Contrib"])
+  expect_equal(as.vector(signif(mca$ind$cos2[,5],2)),
+               data.frame(res$ind)[res$ind$Type == "Active" & res$ind$Axis == "5", "Cos2"])
+})  
+  
+test_that("Supplementary individuals results are equal", {
+  expect_equal(as.vector(signif(mca$ind.sup$coord[,4],3)),
+               data.frame(res$ind)[res$ind$Type == "Supplementary" & res$ind$Axis == "4", "Coord"])
+  expect_equal(as.vector(signif(mca$ind.sup$cos2[,2],2)),
+               data.frame(res$ind)[res$ind$Type == "Supplementary" & res$ind$Axis == "2", "Cos2"])
+})  
+
