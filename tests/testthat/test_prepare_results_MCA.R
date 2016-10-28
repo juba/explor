4221faea6f6154775a8c6fbaddee5c0a1f855f5b
Julien Barnier <julien@nozav.org>
2016-10-28 15:08:45 +0200

    Add excl argument

--- tests/testthat/test_prepare_results_MCA.R
+++ tests/testthat/test_prepare_results_MCA.R
@@ -2,8 +2,9 @@ library(FactoMineR)
 context("prepare_results.MCA")
 
 data(hobbies)
-mca <- MCA(hobbies[1:1000,c(1:8,21:23)],quali.sup = 9:10, 
-           quanti.sup = 11, ind.sup = 1:100, graph = FALSE)
+mca <- MCA(hobbies[1:1000,c(1:8,21:23)], quali.sup = 9:10, 
+           quanti.sup = 11, ind.sup = 1:100, excl = c(5, 8),
+           graph = FALSE)
 res <- prepare_results(mca)
 
 test_that("Eigenvalues are equals", {
