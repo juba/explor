c8b2b0dbfb701272574c11b12f8b3242a4bc21a4
Julien <julien@nozav.org>
2021-05-20 15:56:20 +0200

    Add speMCA_varsup function

---
+++ R/utils.R
@@ -0,0 +1,32 @@
+#' Compute supplementary variables data for a GDAtools::speMCA result
+#'
+#' @param mca result object from speMCA.
+#' @param df data frame with the supplementary variables data. Must have the
+#' same number of rows than the data used with speMCA.
+#'
+#' @return
+#' A list of results suitable to be added as a `supv` element to the `mca`
+#' object.
+#'
+#' @seealso
+#' \code{\link[GDAtools]{speMCA}}, \code{\link[GDAtools]{varsup}}
+
+speMCA_varsup <- function(mca, df) {
+    res <- lapply(names(df), function(name) {
+        l <- GDAtools::varsup(mca, df[, name])
+        l <- lapply(l, function(x) {
+            if (is.data.frame(x)) {
+                rownames(x) <- paste(name, rownames(x), sep = "____")
+            }
+            x
+        })
+        l
+    })
+    Reduce(function(acc, cur) {
+        for (name in names(acc)) {
+            if (name == "weight") next
+            acc[[name]] <- dplyr::bind_rows(acc[[name]], cur[[name]])
+        }
+        acc
+    }, res)
+}
