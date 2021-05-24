bed120945f85028ed3234c33c86b0f62a192ffd0
Julien <julien@nozav.org>
2021-05-24 19:13:03 +0200

    Formatting

--- example.R
+++ example.R
@@ -9,15 +9,14 @@ library(explor)
 
 data(hdv2003)
 
-d <- hdv2003 %>% 
+d <- hdv2003 %>%
   select(sexe, qualif, relig, cuisine, bricol, cinema, sport, age, freres.soeurs)
 acm <- MCA(d, quali.sup = 6:7, ind.sup = 1:50, quanti.sup = 8:9, graph = FALSE)
 explor(acm)
 
-d <- hdv2003 %>% 
+d <- hdv2003 %>%
   select(sexe, nivetud, qualif, clso, relig, cuisine, bricol)
 acm <- MCA(d, graph = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
 explor(acm)
 
 ## MCA 2
@@ -26,9 +25,8 @@ library(FactoMineR)
 library(explor)
 
 data(hobbies)
-mca <- MCA(hobbies[1:1000,c(1:8,21:23)],quali.sup = 9:10, quanti.sup = 11, ind.sup = 1:100, graph = FALSE)
-#mca <- MCA(hobbies[1:1000,c(1:8,21:22)],quali.sup = 9:10, ind.sup = 1:100, graph = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
+mca <- MCA(hobbies[1:1000, c(1:8, 21:23)], quali.sup = 9:10, quanti.sup = 11, ind.sup = 1:100, graph = FALSE)
+# mca <- MCA(hobbies[1:1000,c(1:8,21:22)],quali.sup = 9:10, ind.sup = 1:100, graph = FALSE)
 explor(mca)
 
 ## PCA
@@ -37,9 +35,8 @@ library(FactoMineR)
 library(explor)
 
 data(decathlon)
-d <- decathlon[,1:12]
-pca <- PCA(d, quanti.sup = 11:12,  ind.sup = 1:4, graph = FALSE, scale.unit = TRUE)
-detach(package:explor, unload=TRUE); library(explor)
+d <- decathlon[, 1:12]
+pca <- PCA(d, quanti.sup = 11:12, ind.sup = 1:4, graph = FALSE, scale.unit = TRUE)
 explor(pca)
 
 
@@ -51,8 +48,7 @@ library(explor)
 data(decathlon)
 d <- decathlon
 d$sexe <- sample(c("Homme", "Femme"), 41, replace = TRUE)
-pca <- PCA(d, quanti.sup = 11:12,  quali.sup = 13:14, ind.sup = 1:4, graph = FALSE, scale.unit = TRUE)
-detach(package:explor, unload=TRUE); library(explor)
+pca <- PCA(d, quanti.sup = 11:12, quali.sup = 13:14, ind.sup = 1:4, graph = FALSE, scale.unit = TRUE)
 explor(pca)
 
 
@@ -64,20 +60,17 @@ library(questionr)
 
 data(children)
 res.ca <- CA(children[1:14, 1:5], graph = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
 explor(res.ca)
 
-  
+
 data(children)
 res.ca <- CA(children, row.sup = 15:18, col.sup = 6:8, graph = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
 explor(res.ca)
 
 data(children)
 tmp <- children
-tmp[,9] <- factor(sample(c("red","blue","green"), 18, replace=TRUE))  
+tmp[, 9] <- factor(sample(c("red", "blue", "green"), 18, replace = TRUE))
 res.ca <- CA(tmp, row.sup = 15:18, col.sup = 6:8, quali.sup = 9, graph = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
 explor(res.ca)
 
 
@@ -95,13 +88,11 @@ sup_ind <- d[1:10, -(8:9)]
 pca <- dudi.pca(d[-(1:10), -(8:9)], scale = TRUE, scannf = FALSE, nf = 5)
 pca$supi <- suprow(pca, sup_ind)
 pca$supv <- supcol(pca, dudi.pca(sup_var, scale = TRUE, scannf = FALSE)$tab)
-detach(package:explor, unload=TRUE); library(explor)
 explor(pca)
 
 library(ade4)
 data(deug)
 pca <- dudi.pca(deug$tab, scale = TRUE, scannf = FALSE, nf = 5)
-detach(package:explor, unload=TRUE); library(explor)
 explor(pca)
 
 ## MCA
@@ -109,15 +100,14 @@ explor(pca)
 library(explor)
 library(ade4)
 data(banque)
-d <- banque[-(1:100),-(19:21)]
+d <- banque[-(1:100), -(19:21)]
 ind_sup <- banque[1:100, -(19:21)]
-var_sup <- banque[-(1:100),19:21]
+var_sup <- banque[-(1:100), 19:21]
 acm <- dudi.acm(d, scannf = FALSE, nf = 5)
 ## Supplementary variables
 acm$supv <- supcol(acm, dudi.acm(var_sup, scannf = FALSE, nf = 5)$tab)
 ## Supplementary individuals
 acm$supi <- suprow(acm, ind_sup)
-detach(package:explor, unload=TRUE); library(explor)
 explor(acm)
 
 ## CA
@@ -127,17 +117,15 @@ library(explor)
 
 data(bordeaux)
 tab <- bordeaux
-row_sup <- tab[5,-4]
-col_sup <- tab[-5,4]
-coa <- dudi.coa(tab[-5,-4], nf = 5, scannf = FALSE)
+row_sup <- tab[5, -4]
+col_sup <- tab[-5, 4]
+coa <- dudi.coa(tab[-5, -4], nf = 5, scannf = FALSE)
 coa$supr <- suprow(coa, row_sup)
 coa$supc <- supcol(coa, col_sup)
-detach(package:explor, unload=TRUE); library(explor)
 explor(coa)
 
 data(bordeaux)
 coa <- dudi.coa(bordeaux, nf = 5, scannf = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
 explor(coa)
 
 
@@ -148,7 +136,7 @@ explor(coa)
 library(explor)
 library(GDAtools)
 data(Music)
-mca <- speMCA(Music[,1:5],excl=c(3,6,9,12,15))
+mca <- speMCA(Music[, 1:5], excl = c(3, 6, 9, 12, 15))
 explor(mca)
 
 
@@ -156,10 +144,10 @@ explor(mca)
 library(explor)
 library(GDAtools)
 data(Music)
-getindexcat(Music[,1:4])
+getindexcat(Music[, 1:4])
 mca <- speMCA(Music[3:nrow(Music), 1:4], excl = c(3, 6, 9, 12))
 mca$supi <- indsup(mca, Music[1:2, 1:4])
-mca$supv <- speMCA_varsup(mca, Music[3:nrow(Music), 5:6])
+mca$supv <- speMCA_varsup(mca, Music[3:nrow(Music), 5, drop = FALSE])
 explor(mca)
 
 
@@ -172,11 +160,10 @@ library(explor)
 tmp <- farms[4:20, 2:4]
 mca <- MASS::mca(tmp, nf = 11)
 supi_df <- farms[1:3, 2:4]
-supi <- predict(mca, supi_df, type="row")
+supi <- predict(mca, supi_df, type = "row")
 rownames(supi) <- rownames(supi_df)
 mca$supi <- supi
-mca$supv <- predict(mca, farms[4:20, 1, drop=FALSE], type="factor")
-detach(package:explor, unload=TRUE); library(explor)
+mca$supv <- predict(mca, farms[4:20, 1, drop = FALSE], type = "factor")
 explor(mca)
 
 
@@ -186,26 +173,22 @@ explor(mca)
 
 tmp <- USArrests
 pca <- princomp(tmp, cor = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
 explor(pca)
 
-tmp <- USArrests[6:50,]
+tmp <- USArrests[6:50, ]
 pca <- princomp(tmp, cor = TRUE)
-pca$supi <- predict(pca, USArrests[1:5,])
-detach(package:explor, unload=TRUE); library(explor)
+pca$supi <- predict(pca, USArrests[1:5, ])
 explor(pca)
 
 # prcomp
 
 tmp <- USArrests
 pca <- prcomp(tmp, scale. = FALSE)
-detach(package:explor, unload=TRUE); library(explor)
 explor(pca)
 
-tmp <- USArrests[6:50,]
+tmp <- USArrests[6:50, ]
 pca <- prcomp(tmp, scale. = TRUE)
-pca$supi <- predict(pca, USArrests[1:5,])
-detach(package:explor, unload=TRUE); library(explor)
+pca$supi <- predict(pca, USArrests[1:5, ])
 explor(pca)
 
 
